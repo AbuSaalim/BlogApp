921a670d30befced1161c9ec1068b08c4dbe9b5c
Abu Saalim <salim.whitestone.com>
2024-02-18 22:07:56 +0530

    add a middleware and a function to handle errors

--- api/index.js
+++ api/index.js
@@ -1,25 +1,36 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import userRoute from './routes/user.route.js';
-import authRoute from './routes/auth.route.js';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoute from "./routes/user.route.js";
+import authRoute from "./routes/auth.route.js";
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(() => {
-    console.log('😁 MongoDB is connected');
-}).catch((err) => {
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("✔ 😁 MongoDB is connected ✔");
+  })
+  .catch((err) => {
     console.log(err);
-});
+  });
 
 const app = express();
 app.use(express.json());
 
-app.use('/api/user', userRoute);
-app.use('/api/auth', authRoute);
+app.use("/api/user", userRoute);
+app.use("/api/auth", authRoute);
 
-
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || "500 Code Galat hai Bhaya";
+  const message = err.message || "Internal Server Error";
+  res.status(500).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
 
 app.listen(3000, () => {
-    console.log('✔ 😆 Server is Running on port 3000 ✔');
+  console.log("✔ 😆 Server is Running on port 3000 ✔");
 });
