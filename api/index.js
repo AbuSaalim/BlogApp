dd07f25df42bd5bf7eb5708cbb111ca474c7a6a8
Abu Saalim <salim.whitestone.com>
2024-02-18 17:27:59 +0530

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,9 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRoute from './routes/user.route.js'
+
+
 
 dotenv.config();
 
@@ -15,3 +18,5 @@ const app = express();
 app.listen(3000, () => {
     console.log('✔ 😆 Server is Running on port 3000 ✔');
 });
+
+app.use('/api/user', userRoute);
