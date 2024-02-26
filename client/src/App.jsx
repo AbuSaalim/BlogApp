1d39a35e3333ef28cfe5605221015be0e7e6a5e6
Abu Saalim <salim.whitestone.com>
2024-02-26 15:34:44 +0530

    Create and add Footer Components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,29 @@
-import React from 'react';
-import Home from './pages/Home';
-import About from './pages/About';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
-import Dashboard from './pages/Dashboard';
-import Projects from './pages/Projects'
-import {BrowserRouter, Routes ,Route} from 'react-router-dom';
-import Header from './components/Header';
+import React from "react";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <BrowserRouter>
-    <Header />
+      <Header />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/sign-in' element={<SignIn />} />
-        <Route path='/sign-up' element={<SignUp />} />
-        <Route path='/dashboard' element={<Dashboard />} />
-        <Route path='/project' element={<Projects />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/project" element={<Projects />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
-
-
+export default App;
